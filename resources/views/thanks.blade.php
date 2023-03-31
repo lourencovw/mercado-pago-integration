fbd96a5a59fb195047ead7a7015a88a65987ae51
Lourenco Werneck <lourencovw@gmail.com>
2023-03-31 15:20:38 -0300

    Add 'thanks' page

---
+++ resources/views/thanks.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    @vite('resources/css/app.css')
+    <script src="https://sdk.mercadopago.com/js/v2"></script>
+
+
+</head>
+
+<body class="antialiased">
+    <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+
+        <div class="max-w-7xl mx-auto p-6 lg:p-8">
+
+            <div class="mt-16">
+                <h1 class="text-white" >Obrigado!!</h1>
+
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
